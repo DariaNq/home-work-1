4a40e2217b24b44034d47e6669b3f6bf11724c20
Darina <darina.vaxrusheva@bk.ru>
2022-07-03 18:12:38 +0300

    homework

--- Program.cs
+++ Program.cs
@@ -35,14 +35,73 @@
 
 // Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
-Console.Write("enter a number: ");
-int number = Convert.ToInt32(Console.ReadLine());
-if (number % 2 == 0)
-    Console.Write("Вы ввели четное число");
+//Console.Write("enter a number: ");
+//int number = Convert.ToInt32(Console.ReadLine());
+// if (number % 2 == 0)
+//    Console.Write("Вы ввели четное число");
+//
+//else
+ //   Console.Write("Вы ввели нечетное число");    
 
-else
-    Console.Write("Вы ввели нечетное число");    
 
+// Напишите программу которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру из числа
+
+//void randomNumber(int min, int max){
+//int number = new Random().Next(min, max+1);
+// Console.WriteLine(number);
+//onsole.Write(number + " -> ");
+//if (number / 10 > number % 10){
+  //  Console.WriteLine(number / 10);
+//}
+//else{
+    //Console.WriteLine(number % 10);
+//}
+//}
+//Console.WriteLine("Введите минимальное число диапазона: ");
+//int start = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите максимальное число диапазона: ");
+//int end = Convert.ToInt32(Console.ReadLine());
+//randomNumber(start, end); 
+
+//int SumTwoNumbers( int a, int b){
+//    int sum = a + b;
+ //   return sum;
+//}
+
+//Console.WriteLine("Введите первое число: ");
+//int number1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите второе число: ");
+//int number2= Convert.ToInt32(Console.ReadLine());
+//int s = SumTwoNumbers(number1, number2);
+//Console.WriteLine("Сумма двух чисел: " + s);
+
+
+//Задача 10. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+//Console.WriteLine("Введите любое трехзначное число");
+//int number = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(number);
+//int num1 = number / 10;
+//int num2 = num1 % 10;
+//Console.WriteLine(number + "->" + num2);
+
+//Задача 15. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+//Console.WriteLine("Введите любое целое число");
+//int number = Convert.ToInt32(Console.ReadLine());
+//if (number > 7){
+ //  Console.WriteLine("Ввели некорректное число");
+//}
+//else 
+//    if (number <= 5){
+//      Console.WriteLine(number + " -> " + "Рабочий день");
+//    }
+//    else
+//    Console.WriteLine(number + " -> " + "Выходной день");
+    
+
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+   
 
 
 
