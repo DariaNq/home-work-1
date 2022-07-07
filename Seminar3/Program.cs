4aef828482ef47ab8592c5a03ee12bc22bb7dfbe
Darina <darina.vaxrusheva@bk.ru>
2022-07-07 15:17:51 +0300

    HomeWork

---
+++ Seminar3/Program.cs
@@ -0,0 +1,57 @@
+﻿//Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+//Console.Write("Введите число: ");
+//string? number = Console.ReadLine();
+
+//void VerificationNumber(string number){
+ // if (number[0]==number[4] || number[1]==number[3]){
+ //   Console.WriteLine($"Ваше число: {number} - палиндром.");
+ // }
+ // else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+//}
+
+//if (number!.Length == 5){
+ // VerificationNumber(number);
+//}
+//else Console.WriteLine($"Введи правильное число");
+
+
+//Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+//double SolutionSpace (int x1, int y1, int c1, int x2, int y2, int c2){
+ //   return Math.Sqrt((x2 - x1)*(x2 - x1)+(y2 - y1)*(y2 - y1)+(c2 - c1)*(c2 - c1));
+//}
+//   Console.WriteLine("Введите координату х1");
+//   int x1 = Convert.ToInt32(Console.ReadLine());
+//   Console.WriteLine("Введите координату y1");
+//   int y1 = Convert.ToInt32(Console.ReadLine());
+//   Console.WriteLine("Введите координату c1");
+//   int c1 = Convert.ToInt32(Console.ReadLine());
+//   Console.WriteLine("Введите координату x2");
+//   int x2 = Convert.ToInt32(Console.ReadLine());
+ //  Console.WriteLine("Введите координату y2");
+//  int y2 = Convert.ToInt32(Console.ReadLine());
+ //  Console.WriteLine("Введите координату c2");
+ //  int c2 = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine(SolutionSpace (x1, y1, c1, x2, y2, c2));
+
+
+//Задача 23.Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+//Console.Write("Input number:");
+//int number = Convert.ToInt32(Console.ReadLine());
+//Console.Write (number + " -> ");
+ //  if (number >= 0)
+//   {
+//    for (int current = 0; current <= number; current ++)
+//        Console.Write(current*current*current + " ");
+//   }
+//  else
+//  {
+//    for (int current = -1; current >= number; current --)
+//       Console.Write(current*current*current + " ");
+//  }
+
+
+
